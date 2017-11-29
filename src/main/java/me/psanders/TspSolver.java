61ceca7ccba3867e8c8de800716e67721113cbb8
hxtk <peter@psanders.me>
2017-11-29 13:56:24 -0500

    Output formatting

--- src/main/java/me/psanders/TspSolver.java
+++ src/main/java/me/psanders/TspSolver.java
@@ -3,17 +3,30 @@
 
 package me.psanders;
 
-import me.psanders.graph.path.Path;
+import me.psanders.graph.path.Cycle;
+
+import java.text.DecimalFormat;
+import java.util.Iterator;
 
 class TspSolver {
 
   public static void main(String[] args) {
-    Path path = new ShortRouteFinder(args).getRoute();
+    Cycle<String, Long> path = new ShortRouteFinder(args).getRoute();
     if (path == null) {
       System.out.println("An unexpected error has occurred and no route was found.");
       System.exit(2);
     }
 
-    System.out.println(path);
+    // Print all of the destinations in the order prescribed by the router
+    for (Object place: path.getOrder()) {
+      System.out.println(place + " ->");
+    }
+
+    // Print the first place again because this is a cycle, i.e., it returns to its starting point.
+    System.out.println(path.getOrder().get(0));
+
+    double cost = ((double) (path.getCost())) / 1609.344/*meters per mile*/;
+    DecimalFormat df = new DecimalFormat("#.#");
+    System.out.println("Total distance: " + df.format(cost) + "mi");
   }
 }
