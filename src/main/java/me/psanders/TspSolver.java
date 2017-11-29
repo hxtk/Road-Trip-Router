36a023c6a1b226af9d20d31b0af8498fbc2f02d3
hxtk <peter@psanders.me>
2017-11-29 13:23:17 -0500

    Remove unneded exception handling

--- src/main/java/me/psanders/TspSolver.java
+++ src/main/java/me/psanders/TspSolver.java
@@ -7,8 +7,13 @@ import me.psanders.graph.path.Path;
 
 class TspSolver {
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     Path path = new ShortRouteFinder(args).getRoute();
+    if (path == null) {
+      System.out.println("An unexpected error has occurred and no route was found.");
+      System.exit(2);
+    }
+
     System.out.println(path);
   }
 }
