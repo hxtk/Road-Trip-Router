b0a2c478c4e0713afafc41387d73e66da5bd8da5
hxtk <peter@psanders.me>
2017-11-29 13:17:40 -0500

    Handle unresolvable places

--- src/main/java/me/psanders/ShortRouteFinder.java
+++ src/main/java/me/psanders/ShortRouteFinder.java
@@ -3,9 +3,13 @@
 
 package me.psanders;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Random;
 
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DistanceMatrixElement;
+import com.google.maps.model.DistanceMatrixElementStatus;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.ParseException;
@@ -30,7 +34,7 @@ public class ShortRouteFinder {
     this.args = args;
   }
 
-  public Cycle getRoute() throws ParseException {
+  public Cycle getRoute() {
     Options options = new Options();
     options.addOption("k", "key", true, "Google Maps Services API Key");
     options.addOption("h", "help", false, "Print this usage message");
@@ -40,29 +44,29 @@ public class ShortRouteFinder {
     //options.addOption("no-fee", false, "Selects route that avoids toll roads and ferries");
     //options.addOption("scenic", false, "Selects route that avoids highways and toll roads");
 
-    CommandLine flags = new GnuParser().parse(options, args);
-
-    // Print help if they asked for it
-    if (flags.hasOption("help")) {
-      new HelpFormatter().printHelp("Road Trip Router",
-          "Find a (nearly) optimal route for your next road trip", options,
-          "Please report any bugs or comments to https://github.com/hxtk/Road-Trip-Router",
-          true);
-      System.exit(0);
-    } else if (!flags.hasOption("key")) {  // Verify that a key is provided.
-      new HelpFormatter().printHelp("RoutePlanner",
-          "Find a (nearly) optimal route for your next road trip", options,
-          "Please report any bugs or comments to https://github.com/hxtk/Road-Trip-Router",
-          true);
-      System.exit(1);
-    }
-    String[] places = flags.getArgs();
-
-    GeoApiContext context = new GeoApiContext.Builder().apiKey(flags.getOptionValue("key")).build();
-    DistanceMatrixApiRequest req = DistanceMatrixApi.getDistanceMatrix(
-        context, places, places);
-
     try {
+      CommandLine flags = new GnuParser().parse(options, args);
+      String[] places = flags.getArgs();
+
+      // Print help if they asked for it
+      if (flags.hasOption("help")) {
+        new HelpFormatter().printHelp("Road Trip Router",
+            "Find a (nearly) optimal route for your next road trip", options,
+            "Please report any bugs or comments to https://github.com/hxtk/Road-Trip-Router",
+            true);
+        System.exit(0);
+      } else if (!flags.hasOption("key") || places.length < 2) {
+        new HelpFormatter().printHelp("RoutePlanner",
+            "Find a (nearly) optimal route for your next road trip", options,
+            "Please report any bugs or comments to https://github.com/hxtk/Road-Trip-Router",
+            true);
+        System.exit(1);
+      }
+
+      GeoApiContext context = new GeoApiContext.Builder().apiKey(flags.getOptionValue("key")).build();
+      DistanceMatrixApiRequest req = DistanceMatrixApi.getDistanceMatrix(
+          context, places, places);
+
       DistanceMatrix distMatrix = req.await();
       HashMap<String, Integer> labels = new HashMap<String, Integer>();
 
@@ -71,7 +75,21 @@ public class ShortRouteFinder {
       for (int i = 0; i < places.length; ++i) {
         labels.put(places[i], i);
         for (int j = 0; j < places.length; ++j) {
-          matrix[i][j] = distMatrix.rows[i].elements[j].distance.inMeters;
+          if (i == j) continue;  // Skip the diagonal axis.
+
+          DistanceMatrixElement element = distMatrix.rows[i].elements[j];
+          switch (element.status) {
+            case NOT_FOUND:
+              System.out.println("We couldn't resolve \"" + places[i]
+                  + "\" so we're trying to route around it...");
+              // Fallthrough
+            case ZERO_RESULTS:
+              // The algorithm will optimize away from this leg if the cost is arbitrarily high.
+              matrix[i][j] = matrix[j][i] = Long.MAX_VALUE / 2;
+              break;
+            default:
+              matrix[i][j] = element.distance.inMeters;
+          }
         }
       }
 
@@ -80,7 +98,10 @@ public class ShortRouteFinder {
           new GeneticOptimizationStrategy<String, Long>(new Random()));
 
       return finder.getOptimalCycle();
-    } catch (Exception e) {
+    } catch (ParseException e) {
+      // TODO(hxtk): Exit gracefully on exception.
+      e.printStackTrace();
+    } catch (ApiException | InterruptedException | IOException e) {
       // TODO(hxtk): Exit gracefully on exception.
       e.printStackTrace();
     }
